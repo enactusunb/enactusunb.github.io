3d741ca9e0cb641878b77b55a8b0f7c0037262f5
Henrique Rodrigues Orefice <h.orefice@gmail.com>
2016-04-24 18:12:50 -0300

    clean up done

--- js/main.js
+++ js/main.js
@@ -1,30 +1,19 @@
-// Hello.
-//
-// This is The Scripts used for ___________ Theme
-//
-//
-
-function main() {
-
 (function () {
    'use strict';
 
    /* ==============================================
   	Testimonial Slider
   	=============================================== */ 
-
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 40
-            }, 900);
-            return false;
-          }
-        }
-      });
+  	$('a.page-scroll').click(function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 40
+      }, 900);
+      // Closes the Responsive Menu on Menu Item Click
+      if ($anchor.parents('.navbar-collapse').length)
+        $('.navbar-toggle:visible').click();
+      event.preventDefault();
+    });
 
     /*====================================
     Show Menu on Book
@@ -96,7 +85,3 @@ function main() {
 
 
 }());
-
-
-}
-main();
